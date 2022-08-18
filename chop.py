55c964f35112d5da06065142bf4003d46ecf55c3
Choppy <65018185+choppy-doppy@users.noreply.github.com>
2022-08-18 16:56:22 -0700

    Update chop.py

--- chop.py
+++ chop.py
@@ -2,7 +2,9 @@
 
 #don't know what this does it was added automatically
 from multiprocessing.connection import wait
+#i know what these do
 import time
+from subprocess import Popen
 
 print("you're an idiot lmfaoooo")
 time.sleep(1.5)
@@ -12,6 +14,7 @@ while 1==1:
     your_iq = your_iq - 1
     time.sleep(0.0001)
 
-from subprocess import Popen
-p = Popen("test.bat", cwd=r"c:\Users\pc name lol\Desktop\chop.py")
+    p = Popen("test.bat", cwd=r"c:\Users\pc name lol\Desktop\chop.py")
 stdout, stderr = p.communicate()
+
+# hi nightfall uwu
